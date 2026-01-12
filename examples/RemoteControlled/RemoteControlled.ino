1695c22e520da9e02cf05029c694d58311b02b03
AnDiLi00 <pecuch.andreas@gmail.com>
2026-01-12 13:49:19 +0100

    removed remotexy from library

--- examples/RemoteControlled/RemoteControlled.ino
+++ examples/RemoteControlled/RemoteControlled.ino
@@ -1,10 +1,73 @@
-#include "Esp32Robot.h"
+#include <Esp32Robot.h>
+#include <RemoteXY.h>
 
-void setup()
-{
-  Serial.begin(115200);
+// RemoteXY GUI configuration
+#pragma pack(push, 1)
+uint8_t const PROGMEM RemoteXY_CONF_PROGMEM[] = {
+  255, 7, 0, 2, 0, 186, 0, 19, 0, 0, 0, 0, 31, 1, 200, 84, 2, 3, 2, 8,
+  0, 3, 143, 2, 19, 34, 2, 2, 26, 129, 163, 5, 26, 12, 64, 17, 100, 114, 105, 118,
+  101, 0, 129, 163, 21, 25, 12, 64, 17, 119, 97, 108, 107, 0, 4, 17, 9, 11, 74, 48,
+  2, 26, 4, 57, 41, 74, 11, 176, 2, 26, 129, 6, 1, 33, 12, 64, 17, 115, 112, 101,
+  101, 100, 0, 129, 71, 1, 44, 12, 64, 17, 115, 116, 101, 101, 114, 105, 110, 103, 0, 131,
+  156, 67, 40, 14, 2, 17, 2, 31, 99, 111, 110, 102, 105, 103, 117, 114, 101, 0, 6, 7,
+  0, 131, 156, 67, 40, 14, 2, 17, 2, 31, 98, 97, 99, 107, 0, 9, 4, 4, 43, 74,
+  11, 176, 2, 26, 129, 30, 3, 18, 12, 64, 17, 108, 101, 102, 116, 0, 4, 76, 43, 74,
+  11, 176, 2, 26, 129, 99, 3, 25, 12, 64, 17, 114, 105, 103, 104, 116, 0, 67, 20, 20,
+  40, 10, 125, 2, 26, 67, 93, 20, 40, 10, 125, 2, 26
+};
+
+struct {
+  uint8_t main; // =1 if page is visible, else =0, from 0 to 1
+  uint8_t configure; // =1 if page is visible, else =0, from 0 to 1
+  uint8_t mode; // from 0 to 2
+  int8_t speed; // from -100 to 100
+  int8_t steering; // from -100 to 100
+  int8_t left_offset_slider; // from -100 to 100
+  int8_t right_offset_slider; // from -100 to 100
+
+  // output variables
+  int8_t left_offset; // -128 .. 127
+  int8_t right_offset; // -128 .. 127
+
+  // other variable
+  uint8_t connect_flag; // =1 if wire connected, else =0
+} RemoteXY;
+#pragma pack(pop)
+
+void RemoteXY_OnModeChange() {
 }
 
-void loop()
-{
+void RemoteXY_OnSpeedChange() {
+}
+
+void RemoteXY_OnSteeringChange() {
+}
+
+void RemoteXY_OnOffsetLeftChange() {
+}
+
+void RemoteXY_OnOffsetRightChange() {
+}
+
+Esp32Robot Esp32Robot;
+
+void setup() {
+  Serial.begin(115200);
+
+  RemoteXYGui *gui = RemoteXYEngine.addGui(RemoteXY_CONF_PROGMEM, &RemoteXY);
+  gui->addConnection(new CRemoteXYStream_BLEDevice("ESP32Robot"));
+
+  RemoteXYEngine.addVariableEvent(RemoteXY.mode, RemoteXY_OnModeChange);
+  RemoteXYEngine.addVariableEvent(RemoteXY.speed, RemoteXY_OnSpeedChange);
+  RemoteXYEngine.addVariableEvent(RemoteXY.steering, RemoteXY_OnSteeringChange);
+  RemoteXYEngine.addVariableEvent(RemoteXY.left_offset_slider, RemoteXY_OnOffsetLeftChange);
+  RemoteXYEngine.addVariableEvent(RemoteXY.right_offset_slider, RemoteXY_OnOffsetRightChange);
+  
+  Esp32Robot.OnSetup();
+}
+
+void loop() {
+  RemoteXYEngine.handler();
+  
+  Esp32Robot.OnLoop();
 }
--- src/Esp32Robot.cpp
+++ src/Esp32Robot.cpp
@@ -1,9 +1,3 @@
-#include <vector>
-
-#include <Arduino.h>
-#include <BLEDevice.h>
-#include <Servo.h>
-
 #include "Esp32Robot.h"
 
 const int8_t Esp32Robot::PIN_NOPIN = -1;
@@ -17,95 +11,114 @@ const int8_t Esp32Robot::WALK_DRIVE_DEFAULT = 90;
 const int8_t Esp32Robot::WALK_ANKLE_DEFAULT = 125;
 const int8_t Esp32Robot::WALK_ARM_DEFAULT = 45;
 
-Esp32Robot::Esp32Robot(void) : data()
-{
+Esp32Robot::Esp32Robot(void) :
+  data() {
 }
 
-// Esp32Robot::Esp32Robot(const Esp32Robot &copy) {
-// }
+Esp32Robot::Esp32Robot(const Esp32Robot &copy) :
+  data() {
 
-Esp32Robot::~Esp32Robot(void)
-{
+  for (int8_t i = 0; i < PART_PARTS; i++) {
+    data.pins[i] = copy.data.pins[i];
+    data.offsets[i] = copy.data.offsets[i];
+
+    data.servos[i].detach();
+    if (data.pins[i] != PIN_NOPIN) {
+      data.servos[i].attach(data.pins[i]);
+    }
+  }
 }
 
-// Esp32Robot::Esp32Robot &operator=(const Esp32Robot &other) {
-// }
+Esp32Robot::~Esp32Robot(void) {
+}
 
-int8_t Esp32Robot::GetPin(const BodyParts &part) const
-{
+Esp32Robot &Esp32Robot::operator=(const Esp32Robot &other) {
+  if (&other != this) {
+    for (int8_t i = 0; i < PART_PARTS; i++) {
+      data.pins[i] = other.data.pins[i];
+      data.offsets[i] = other.data.offsets[i];
+
+      data.servos[i].detach();
+      if (data.pins[i] != PIN_NOPIN) {
+        data.servos[i].attach(data.pins[i]);
+      }
+    }
+  }
+
+  return (*this);
+}
+
+int8_t Esp32Robot::GetPin(const BodyParts &part) const {
   return (data.pins[part]);
 }
 
-int8_t Esp32Robot::GetOffset(const BodyParts &part) const
-{
+int8_t Esp32Robot::GetOffset(const BodyParts &part) const {
   return (data.offsets[part]);
 }
 
-void Esp32Robot::SetPin(const BodyParts &part, const int8_t &pin)
-{
-  if (pin != data.pins[part])
-  {
+void Esp32Robot::SetPin(const BodyParts &part, const int8_t &pin) {
+  if (pin != data.pins[part]) {
     data.pins[part] = pin;
+
     data.servos[part].detach();
-    if (data.pins[part] != PIN_NOPIN)
-    {
+    if (data.pins[part] != PIN_NOPIN) {
       data.servos[part].attach(data.pins[part]);
     }
   }
 }
 
-void Esp32Robot::SetOffset(const BodyParts &part, const int8_t &offset)
-{
+void Esp32Robot::SetOffset(const BodyParts &part, const int8_t &offset) {
   data.offsets[part] = offset;
 }
 
-void Esp32Robot::Move(const BodyParts &part, const int8_t &position)
-{
+void Esp32Robot::Move(const BodyParts &part, const int8_t &position) {
   data.servos[part].write(position);
 }
 
-MovementMode Esp32Robot::GetMode(void) const
-{
+Esp32Robot::MovementMode Esp32Robot::GetMode(void) const {
   return (data.mode);
 }
 
-void Esp32Robot::SetMode(const MovementMode &mode)
-{
-  if (mode != data.mode)
-  {
+void Esp32Robot::SetMode(const MovementMode &mode) {
+  if (mode != data.mode) {
     data.mode = mode;
 
-    switch (data.mode)
-    {
-    case MOVE_DRIVE:
-      break;
-    case MOVE_WALK:
-      break;
+    switch (data.mode) {
+      case MOVE_DRIVE:
+        break;
+      case MOVE_WALK:
+        break;
     }
   }
 }
 
-void Esp32Robot::OnSetup(void) const
-{
-  RemoteXY_Init(); // initialization by macros
-
-  RemoteXYEngine.addVariableEvent(RemoteXY.main, RemoteXY_main_event);
-  RemoteXYEngine.addVariableEvent(RemoteXY.configure, RemoteXY_configure_event);
-  RemoteXYEngine.addVariableEvent(RemoteXY.mode, RemoteXY_mode_event);
-  RemoteXYEngine.addVariableEvent(RemoteXY.speed, RemoteXY_speed_event);
-  RemoteXYEngine.addVariableEvent(RemoteXY.steering, RemoteXY_steering_event);
-  RemoteXYEngine.addVariableEvent(RemoteXY.left_offset_slider, RemoteXY_left_offset_slider_event);
-  RemoteXYEngine.addVariableEvent(RemoteXY.right_offset_slider, RemoteXY_right_offset_slider_event);
+void Esp32Robot::OnSetup(void) {
 }
 
-void Esp32Robot::OnLoop(void) const
-{
+void Esp32Robot::OnLoop(void) {
 }
 
-void Esp32Robot::OnEnd(void) const
-{
-  for (size_t i = 0; i < data.servos.size(); i++)
-  {
+void Esp32Robot::OnEnd(void) {
+  for (int8_t i = 0; i < PART_PARTS; i++) {
     data.servos[i].detach();
   }
+}
+
+void Esp32Robot::OnModeChange(const uint8_t &mode) {
+  //Serial.println(mode);
+}
+
+void Esp32Robot::OnSpeedChange(const int8_t &speed) {
+  //Serial.println(speed);
+}
+
+void Esp32Robot::OnSteeringChange(const int8_t &steering) {
+  //Serial.println(steering);
+}
+
+void Esp32Robot::OnOffsetLeftChange(const int8_t &left_offset) {
+  //Serial.println(left_offset);
+}
+void Esp32Robot::OnOffsetRightChange(const int8_t &right_offset) {
+  //Serial.println(right_offset);
 }
