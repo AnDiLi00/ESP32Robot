675fc6b5eddcc9627b6ff3a02e69ab065c892b1c
AnDiLi00 <pecuch.andreas@gmail.com>
2026-01-23 00:15:48 +0100

    fixed matrix animation

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -2,11 +2,15 @@
 
 const uint8_t Matrix::BRIGHTNESS_DEFAULT = 6;
 
-const uint8_t Matrix::UPDATE_CYCLES = 5;
+const uint8_t Matrix::UPDATE_CYCLES = 2;
 const uint8_t Matrix::CYCLES_DEFAULT = 0;
 
 const int8_t Matrix::SIZE = 8;
 const int8_t Matrix::OFFSET_DEFAULT = 0;
+
+const String Matrix::TEST = "Das ist ein Test /$%&+- ";
+const String Matrix::TEST2 = "Test ";
+const String Matrix::TEST3 = "D";
 
 Matrix::Matrix(void) :
   data() {
@@ -71,8 +75,7 @@ void Matrix::OnSetup(const Types::Mood &mood, const Types::MoodSub &submood) {
   data.matrix->setBrightness(BRIGHTNESS_DEFAULT);
   data.matrix->clear();
 
-  String newtext = "Das ist ein Test! ] / ";
-  SetText(newtext.c_str());
+  SetText(TEST.c_str());
 
   data.matrix->writeDisplay();
 }
@@ -86,15 +89,6 @@ void Matrix::OnLoop(const Types::Mood &mood, const Types::MoodSub &submood) {
     data.matrix->clear();
 
     DrawCharacter(data.current, data.offset);
-
-    /*
-    char *next = data.current + 1;
-    if (*next == '\0') {
-      next = (char *)data.text;
-    }
-
-    DrawCharacter(next, data.offset + SIZE);
-    */
 
     if (data.direction != Types::DIR_NO) {
       data.offset++;
@@ -118,45 +112,72 @@ void Matrix::OnEnd(void) {
 }
 
 void Matrix::DrawCharacter(const char *character, const int8_t &offset) {
-  if ((character[0] >= '0') && (character[0] <= '1')) {
-    uint8_t index = (uint8_t)character[0] - (uint8_t)'0';
-    DrawImage(IMG_DIGITS[index], offset);
-  } else if ((character[0] >= 'A') && (character[0] <= 'Z')) {
-    uint8_t index = (uint8_t)character[0] - (uint8_t)'A';
-    DrawImage(IMG_LETTERS_BIG[index], offset);
-  } else if ((character[0] >= 'a') && (character[0] <= 'z')) {
-    uint8_t index = (uint8_t)character[0] - (uint8_t)'a';
-    DrawImage(IMG_LETTERS_SMALL[index], offset);
-  } else {
-    for (uint8_t i = 0; i < IMG_SIGNS_SIZE; i++) {
-      if (character[0] == IMG_SIGNS[i].sign) {
-        DrawImage(IMG_SIGNS[i].image, offset);
-        break;
-      }
-    }
+  uint64_t image = GetImage(character);
+
+  char *next = (char *)character + 1;
+  if (*next == '\0') {
+    next = (char *)data.text;
   }
+  uint64_t image_next = GetImage(next);
+
+  DrawImage(image, image_next, offset);
 }
 
-void Matrix::DrawImage(const uint64_t &image, const int8_t &offset) {
+void Matrix::DrawImage(const uint64_t &image, const uint64_t &image_next, const int8_t &offset) {
   for (int8_t i = 0; i < SIZE; i++) {
-    uint8_t row = (image >> i * SIZE) & 0xFF;
+    uint8_t row = 0x00;
 
     switch (data.direction) {
-      case Types::DIR_UP:
-        row = (row >> offset) & 0xFF;
-        break;
       case Types::DIR_DOWN:
-        row = (row >> offset) & 0xFF;
+      case Types::DIR_UP:
+        {
+          uint8_t r1 = (image >> i * SIZE) & 0xFF;
+          uint8_t r2 = (image_next >> i * SIZE) & 0xFF;
+
+          uint8_t r1_shifted = (data.direction == Types::DIR_UP) ? ((r1 >> offset) & 0xFF) : ((r1 << offset) & 0xFF);
+          uint8_t r2_shifted = (data.direction == Types::DIR_UP) ? ((r2 << (SIZE - offset)) & 0xFF) : ((r2 >> (SIZE - offset)) & 0xFF);
+          
+          row = r1_shifted | r2_shifted;
+        }
         break;
       case Types::DIR_LEFT:
       case Types::DIR_RIGHT:
-        int shifted_i = (data.direction == Types::DIR_LEFT) ? i - offset : i + offset;
-        if ((shifted_i < 0) || (shifted_i >= SIZE)) {
-          continue;
+        {
+          int8_t shifted_i = (data.direction == Types::DIR_RIGHT) ? (i + offset) : (i - offset);
+          if ((shifted_i >= 0) && (shifted_i < SIZE)) {
+            row = (image >> shifted_i * SIZE) & 0xFF;
+          } else {
+            shifted_i = (shifted_i + SIZE) % SIZE;
+            row = (image_next >> shifted_i * SIZE) & 0xFF;
+          }
         }
         break;
     }
 
     data.matrix->displaybuffer[i] = row;
   }
+}
+
+uint64_t Matrix::GetImage(const char *character) {
+  uint64_t image = 0;
+
+  if ((character[0] >= '0') && (character[0] <= '1')) {
+    uint8_t index = (uint8_t)character[0] - (uint8_t)'0';
+    image = IMG_DIGITS[index];
+  } else if ((character[0] >= 'A') && (character[0] <= 'Z')) {
+    uint8_t index = (uint8_t)character[0] - (uint8_t)'A';
+    image = IMG_LETTERS_BIG[index];
+  } else if ((character[0] >= 'a') && (character[0] <= 'z')) {
+    uint8_t index = (uint8_t)character[0] - (uint8_t)'a';
+    image = IMG_LETTERS_SMALL[index];
+  } else {
+    for (uint8_t i = 0; i < IMG_SIGNS_SIZE; i++) {
+      if (character[0] == IMG_SIGNS[i].sign) {
+        image = IMG_SIGNS[i].image;
+        break;
+      }
+    }
+  }
+
+  return (image);
 }
